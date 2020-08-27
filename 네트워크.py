524f017d83a40a971c2349499d093cf46941d07f
YoungmyoungKim <dudaud0205@hanmail.net>
2020-08-28 06:21:00 +0900

    second commit

--- 네트워크.py
+++ 네트워크.py
@@ -1,22 +1,22 @@
 def solution(n, computers):
-    count=0
-    i,j=0,1
-    while True:
-        if i==0 and j==n-1:
-            count+=1
-            break
-        if computers[i][j]==1 and j+1<=n-1:
-            i+=1
-            j+=1
-        elif computers[i][j]==1 and j+1>n-1 :
-            i=0
-        else:
-            count+=1
-            i=0
-    return count
+    stack=[]
+    count=1
+    graph={i: [] for i in range(n)}
+    for i in range(n):
+        for j in range(n):
+            if computers[i][j]==1 and i!=j:
+                graph[i].append(j)
+    paths=map(sorted, [dfs(graph, node) for node in graph])
 
-n1, c1=3, [[1,1,0], [1, 1, 0], [0, 0, 1]]
-n2, c2=3, [[1,1,0], [1, 1, 1], [0, 1, 1]]
+    return len(set([''.join(map(str, path)) for path in paths]))
 
-print(solution(n1, c1))
-print(solution(n2, c2))
+def dfs(graph, start):
+    visited=[]
+    stack=[start]
+
+    while stack:
+        v=stack.pop()
+        if v not in visited:
+            visited.append(v)
+            stack.extend([x for x in graph[v] if x not in visited])
+    return visited
