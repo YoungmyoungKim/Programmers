82d140e9cfc99294a15d0d854f5ed27dc95334c9
YoungmyoungKim <dudaud0205@hanmail.net>
2020-08-28 07:28:03 +0900

    second commit

--- 프린터.py
+++ 프린터.py
@@ -1,17 +1,3 @@
-def solution_1(priorities, location):
-    count=0
-    queue=[(y, x) for x, y in enumerate(priorities)]
-    while len(queue)>0:
-        item=queue.pop(0)
-        if item[0] < max(queue)[0]:
-            queue.append(item)
-        else:
-            count+=1
-            if item[1]==location:
-                return count
-
-
-
 def solution(priorities, location):
     count=0
     priorities=[(y, x) for x, y in enumerate(priorities)]
@@ -29,9 +15,7 @@ p1, l1=[2,1,3,2], 2
 p2, l2=[1, 1, 9, 1, 1, 1], 0
 p3, l3=[1, 1, 9, 1, 1, 1], 5
 
-#print(solution(p1, l1))
-#print(solution(p2, l2))
 
-print(solution_1(p1, l1))
-print(solution_1(p2, l2))
-print(solution_1(p3, l3))
+print(solution(p1, l1))
+print(solution(p2, l2))
+print(solution(p3, l3))
