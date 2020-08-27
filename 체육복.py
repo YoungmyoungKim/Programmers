c5c35415fb79addc7d0dba0f0d94232e280ef708
YoungmyoungKim <dudaud0205@hanmail.net>
2020-08-28 07:12:29 +0900

    second commit

--- 체육복.py
+++ 체육복.py
@@ -1,36 +1,16 @@
-
 def solution(n, lost, reserve):
-    answer=0
-    students=[1]*n
-    for i in range(len(reserve)):
-        students[reserve[i]-1]+=1
-    for i in range(len(lost)):
-        students[lost[i]-1]-=1
+    _reserve=[i for i in reserve if i not in lost]
+    _lost=[i for i in lost if i not in reserve]
 
-    for idx, student in enumerate(students):
-        if student==0:
-            if idx==0 and students[idx+1]==2:
-                students[idx]+=1
-                students[idx+1]-=1
-                continue
-            if idx==len(students)-1 and students[idx-1]==2:
-                students[idx]+=1
-                students[idx-1]-=1
-                continue
-            if idx>0 and idx<len(students)-1:
-                if students[idx-1]==2:
-                    students[idx]+=1
-                    students[idx-1]-=1
-                    continue
-                if students[idx+1]==2:
-                    students[idx]+=1
-                    students[idx+1]-=1
-
-    for student in students:
-        if student>=1:
-            answer+=1
-    return answer
-
+    for num in _reserve:
+        r=num+1
+        l=num-1
+        if l in _lost:
+            _lost.remove(l)
+        elif r in _lost:
+            _lost.remove(r)
+    return n-len(_lost)
+    
 n_1, lost_1, reserve_1=5, [2, 4], [1, 3, 5]
 n_2, lost_2, reserve_2=5, [2, 4], [3]
 n_3, lost_3, reserve_3=3, [3], [1]
