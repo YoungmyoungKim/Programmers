56bfaa6f18c6b085444a6ca935b3608169e231d1
YoungmyoungKim <dudaud0205@hanmail.net>
2020-08-28 07:02:19 +0900

    second commit

--- 완주하지못한선수.py
+++ 완주하지못한선수.py
@@ -1,21 +1,4 @@
-def solution_1(participant, completion):  #시간초과
-    temp=participant
-    for i in completion:
-        if i in participant:
-            temp.remove(i)
-    answer = temp[0]
-    return answer
-
-def solution_2(participant, completion):    #근주힌트 but 시간초과
-    participant.sort()
-    completion.sort()
-
-    for i in range(len(completion)):
-        if participant[i]!=completion[i]:
-            answer=participant[i]
-    return answer
-
-def solution_3(participant, completion):
+def solution(participant, completion):
     participant.sort()
     completion.sort()
     answer=participant[-1]
@@ -33,11 +16,3 @@ completion_2=['marina', 'josipa', 'nikola', 'filipa']
 
 participant_3=['mislav', 'stanko', 'mislav', 'ana']
 completion_3=['stanko','ana', 'mislav']
-p=sorted(participant_3)
-c=sorted(completion_3)
-print(p)
-print(c)
-participant_3.sort()
-completion_3.sort()
-print(participant_3)
-print(completion_3)
